7dea03d8ef11aa468f7c387ea966fb503da880d3
Héctor Alejandro Soto García <asoto@grupo-siayec.com.mx>
2023-09-13 18:36:49 -0600

    fail to create subscription

--- src/index.js
+++ src/index.js
@@ -1,33 +1,89 @@
 require('dotenv').config()
-const { ApolloServer } = require('apollo-server')
-const { PubSub } = require('graphql-subscriptions');
-const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
-const { createServer } = require('http');
+//const { ApolloServer } = require('apollo-server')
+const {ApolloServer} = require('@apollo/server')
+const {startStandaloneServer } = require('@apollo/server/standalone')
+
+const { createServer } = require('http')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const { WebSocketServer } =  require('ws')
+const { useServer } =  require('graphql-ws/lib/use/ws')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+
+const { PubSub } = require('graphql-subscriptions')
+
 const typeDefs = require('./graphql/typesDefs')
 const resolvers = require('./graphql/resolvers/index')
 
-
-const connectMongoDB = require('./config/db')
 
 const pubsub = new PubSub();
 
 
 const httpServer = createServer();
+
+
+
+const connectMongoDB = require('./config/db')
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: '/graphql',
+});
+
+const serverCleanup = useServer({ schema }, wsServer);
+
+//console.log(serverCleanup);
+
+//const httpServer = createServer();
 const server = new ApolloServer({
-    typeDefs, 
-    resolvers,
+    schema,
     context: ({req}) => ({req, pubsub}),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    plugins: [
+        // Proper shutdown for the HTTP server.
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+    
+        // Proper shutdown for the WebSocket server.
+        {
+          async serverWillStart() {
+            return {
+              async drainServer() {
+                await serverCleanup.dispose();
+              },
+            };
+          },
+        },
+    ]
 })
 
+const start = async () => {
+    await server.start()
+    await connectMongoDB()
+    httpServer.listen(5000,() => console.log(`server started in port 5000`))
+
+    /*const { url } = await startStandaloneServer(server, {
+        listen: { port: 5000 },
+    });
+
+    connectMongoDB()
+
+    console.log('server started in port ' + url)*/
+}
 
 
-server.listen({port: 5000})
+start()
+
+
+
+/*server.listen({port: 5000})
     .then(async (res) => {
-        server.applyMiddleware({ app: httpServer });
         console.log(`Server listening on ${res.url}`)
         await connectMongoDB() 
     })
-    .catch(error => console.error('error',error))
+    .catch(error => console.error('error',error))*/
 
 
